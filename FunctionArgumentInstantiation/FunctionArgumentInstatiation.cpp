ce182f966d43441e4070921bed8a34bd70c0b11f
Jiahao Wu <visylar@gmail.com>
2016-04-25 18:06:37 -0400

    now we are able to clone function

--- FunctionArgumentInstantiation/FunctionArgumentInstatiation.cpp
+++ FunctionArgumentInstantiation/FunctionArgumentInstatiation.cpp
@@ -19,52 +19,58 @@ struct FAI : public ModulePass {
     static char ID;
     FAI() : ModulePass(ID) {}
 
-    virtual bool runOnModule(Module &M) {
-        bool modified = false;
-        Module::FunctionListType &functions = M.getFunctionList();
-        for(Module::FunctionListType::iterator function = functions.begin(); function != functions.end(); function++) {
-            errs() << *function << '\n';
-            modified = runOnFunction(*function);
-        }
-        return modified;
-    }
-
-    virtual bool runOnFunction(Function &F) {
-        bool modified = false;
-        int functionCallIndex = 1;
-        for(Function::iterator block = F.begin(); block != F.end(); block++) {
-            for(BasicBlock::iterator inst = block->begin(); inst != block->end(); inst++) {
-                if(isa<CallInst>(inst)) {
-                    errs() << "found call #" << functionCallIndex++ << '\n';
-                    CallInst* callInst = dyn_cast<CallInst>(inst);
-                    Function *callee = callInst->getCalledFunction();
-                    for(Function::arg_iterator arg = callee->arg_begin(); arg != callee->arg_end(); arg++) {
-                        errs() << "==> " << *arg << ' ';
-                    }
-                    errs() << '\n';
-                    std::set<Value *> constantArgs;
-                    // processing caller arguments
-                    for (unsigned int i = 0; i < inst->getNumOperands(); ++i) {
-                        if(isa<Constant>(inst->getOperand(i))) {
-                            errs() << "found a constant argument\n";
-                            constantArgs.insert(inst->getOperand(i));
-                        }
-                    }
-
-                    ValueToValueMapTy VMap;
-                    if(!constantArgs.empty()) {
-                        // Clone function
-                        //Function *cloneFunction = CloneFunction(callee, VMap, false);
-                        //callee->getParent()->getFunctionList().push_back(cloneFunction);
-                        //modified = true;
-                    }
-                }
-            }
-        }
-        return modified;
-    }
+    virtual bool runOnModule(Module &M) override;
+    virtual bool runOnFunction(Function &F);
 };
 }
 
 char FAI::ID = 0;
 static RegisterPass<FAI> X("hello", "Function Argument Instantiation", false, false);
+
+bool FAI::runOnModule(Module &M) {
+    bool modified = false;
+    std::vector<Function *> functions;
+    for(Module::iterator FI = M.begin(); FI != M.end(); FI++) {
+        if (!FI->isDeclaration()) {
+            errs() << *FI << '\n';
+            modified = runOnFunction(*FI);
+        }
+    }
+    return modified;
+}
+
+bool FAI::runOnFunction(Function &F) {
+    bool modified = false;
+    int functionCallIndex = 1;
+    for(Function::iterator block = F.begin(); block != F.end(); block++) {
+        for(BasicBlock::iterator inst = block->begin(); inst != block->end(); inst++) {
+            if(isa<CallInst>(inst)) {
+                errs() << "found call #" << functionCallIndex++ << '\n';
+                CallInst* callInst = dyn_cast<CallInst>(inst);
+                Function *callee = callInst->getCalledFunction();
+                for(Function::arg_iterator arg = callee->arg_begin(); arg != callee->arg_end(); arg++) {
+                    errs() << "==> " << *arg << ' ';
+                }
+                errs() << '\n';
+                std::set<Value *> constantArgs;
+                // processing caller arguments
+                for (unsigned int i = 0; i < inst->getNumOperands(); ++i) {
+                    if(isa<Constant>(inst->getOperand(i))) {
+                        errs() << "found a constant argument\n";
+                        constantArgs.insert(inst->getOperand(i));
+                    }
+                }
+
+                if(!constantArgs.empty() && !callee->isDeclaration()) {
+                    // Clone function
+                    ValueToValueMapTy VMap;
+                    Function* duplicateFunction = CloneFunction(callee, VMap, false);
+                    duplicateFunction->setLinkage(GlobalValue::InternalLinkage);
+                    callee->getParent()->getFunctionList().push_back(duplicateFunction);
+                    modified = true;
+                }
+            }
+        }
+    }
+    return modified;
+}
