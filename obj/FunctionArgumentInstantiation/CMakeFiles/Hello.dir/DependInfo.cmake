60c893e02a0e5c999d1027defcd5604ed12409d0
Jiahao Wu <visylar@gmail.com>
2016-05-05 00:31:27 -0400

    test transformed code put everthing together

---
+++ obj/FunctionArgumentInstantiation/CMakeFiles/Hello.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jiahao/workspace/enee645/src/FunctionArgumentInstantiation/FunctionArgumentInstatiation.cpp" "/home/jiahao/workspace/enee645/src/obj/FunctionArgumentInstantiation/CMakeFiles/Hello.dir/FunctionArgumentInstatiation.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ obj/FunctionArgumentInstantiation/CMakeFiles/Hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Hello.dir/FunctionArgumentInstatiation.cpp.o"
+  "libHello.pdb"
+  "libHello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
